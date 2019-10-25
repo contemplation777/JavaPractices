4c8d00295f1363602bf75fecc6ce4013675526d2
contemplation777 <2228954150@qq.com>
2019-10-25 20:33:49 +0800

    打印short 和long的大小

---
+++ 1025/ShortLong.java
@@ -0,0 +1,13 @@
+public class ShortLong{ 
+	public static void main( String[] args){
+		
+		short minShort = Short.MIN_VALUE;
+		short maxShort = Short.MAX_VALUE;
+		System.out.println(minShort);
+		System.out.println(maxShort);
+		long minLong = Long.MIN_VALUE;
+		long maxLong = Long.MAX_VALUE;
+		System.out.println(minLong);
+		System.out.println(maxLong);
+	}
+}
