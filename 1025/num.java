23ef3738317b5dcea8357aedd02a39d2e316f40e
contemplation777 <2228954150@qq.com>
2019-10-25 21:40:11 +0800

    不同类型

---
+++ 1025/num.java
@@ -0,0 +1,14 @@
+public class num {
+	public static void main(String[] args){
+		int num =  10;
+		long num1 = 10L;
+		int num2 = 077;
+		int num3 = 0b01111;
+		int num4 = 0xAB;
+		System.out.println(num);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+	}
+}
